4da2d51e2dfd3980801765d10c31c1c5c0f2fccf
Jolin816 <125181339+Jolin816@users.noreply.github.com>
2023-07-06 16:29:55 -0400

    add intro and pic

---
+++ src/component/Intro.jsx
@@ -0,0 +1,15 @@
+import '/Users/jolin/git/Blog/src/stylesheets/landingpage.css'
+
+function Intro() {
+    return(
+        <>
+            <img 
+                src='src/assets/img/profile_pic.png'
+                className='profile_pic'
+            />
+            <h1 className='hello'>Hey! It's Jo</h1>
+        </>
+    )
+}
+
+export default Intro
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import '/Users/jolin/git/Blog/src/stylesheets/landingpage.css'
+import Intro from '/Users/jolin/git/Blog/src/component/Intro.jsx'
+
+function LandingPage() {
+    return(
+        <>
+            <Intro />
+        </>
+    )
+}
+
+export default LandingPage
